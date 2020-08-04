cee9dcf0790a3359f1179be28c6ddc7c0f38255e
Ricardo Martins <xrsmartins@hotmail.com>
2020-08-04 11:21:14 -0300

    tabela estudante

---
+++ estudante/core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class estudante(models.Model):
+	
+	name = models.CharField('Nome',max_length=100)
+	telefone = models.CharField('Telefone',max_length=100)
+	course = models.CharField('Curso',max_length=100)
+	create_at = models.DateTimeField(
+		'Criando em',auto_now_add=True, null=True, blank=True
+	)
+	update_at = models.DateTimeField(
+		'Atualizado em',auto_now=True, null=True, blank=True
+	)	
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Estudante'
+		verbose_name_plural = 'Estudantes'
+		ordering = ['name']	
